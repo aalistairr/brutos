3e38fa5225ab87b517873d98dbbb76b84e2f16f2
Alistair Geesing <alistair.geesing@icloud.com>
2020-02-08 01:43:35 +0100

    Implement cpuid values using named structs

--- platform/pc/src/cpuid.rs
+++ platform/pc/src/cpuid.rs
@@ -1,13 +1,47 @@
-use brutos_util_macros::{bitfield, ConvertInner};
+use brutos_util_macros::bitfield;
 
-pub fn cpuid(leaf: u32, subleaf: u32) -> [u32; 4] {
+pub struct Cpuid {
+    pub eax: u32,
+    pub ebx: u32,
+    pub ecx: u32,
+    pub edx: u32,
+}
+
+pub fn cpuid(leaf: u32, subleaf: u32) -> Cpuid {
     unsafe {
         let eax: u32;
         let ebx: u32;
         let ecx: u32;
         let edx: u32;
         asm!("cpuid" : "={eax}" (eax), "={ebx}" (ebx), "={ecx}" (ecx), "={edx}" (edx) : "{eax}" (leaf), "{ecx}" (subleaf) : "memory" : "volatile");
-        [eax, ebx, ecx, edx]
+        Cpuid { eax, ebx, ecx, edx }
+    }
+}
+
+macro_rules! leaf_value {
+    ($name:ident { $($fields:tt)* }) => {
+        bitfield! {
+            #[derive(Copy, Clone, PartialEq, Eq, Debug)]
+            pub struct $name {
+                eax: u32,
+                ebx: u32,
+                ecx: u32,
+                edx: u32,
+            }
+
+            $($fields)*
+        }
+
+        impl From<Cpuid> for $name {
+            fn from(cpuid: Cpuid) -> $name {
+                $name {
+                    eax: cpuid.eax,
+                    ebx: cpuid.ebx,
+                    ecx: cpuid.ecx,
+                    edx: cpuid.edx,
+                }
+            }
+        }
     }
 }
 
@@ -31,14 +65,11 @@ pub mod leaf {
     leaf!(InvariantTsc: super::InvariantTsc = 0x80000007);
 }
 
-bitfield! {
-    #[derive(Copy, Clone, PartialEq, Eq, Debug, ConvertInner)]
-    pub struct CoreCrystalClock([u32; 4]);
-
-    #[ro] field tsc_ratio_den: usize = 0[0..32];
-    #[ro] field tsc_ratio_num: usize = 1[0..32];
-    #[ro] field ccc_freq_raw: usize = 2[0..32];
-}
+leaf_value!(CoreCrystalClock {
+    #[ro] field tsc_ratio_den: usize = eax[0..32];
+    #[ro] field tsc_ratio_num: usize = ebx[0..32];
+    #[ro] field ccc_freq_raw: usize = ecx[0..32];
+});
 
 impl CoreCrystalClock {
     pub fn ccc_freq(&self) -> Option<usize> {
@@ -56,9 +87,6 @@ impl CoreCrystalClock {
     }
 }
 
-bitfield! {
-    #[derive(Copy, Clone, PartialEq, Eq, Debug, ConvertInner)]
-    pub struct InvariantTsc([u32; 4]);
-
-    #[ro] pub field available: bool = 3[8];
-}
+leaf_value!(InvariantTsc {
+    #[ro] pub field available: bool = edx[8];
+});
